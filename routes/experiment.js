be8e7863456b715d064189c4694ae326519c2563
Martin <91martinh@gmail.com>
2015-12-07 17:31:21 +0100

    queue working

--- routes/experiment.js
+++ routes/experiment.js
@@ -5,61 +5,6 @@ var check = require("check-type").init();
 
 var Experiment = require('../models/experiment');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('pages/experiment', { title: 'About' });
-});
-
-/* POST home page. */
-router.post('/', function(req, res, next) {
-    if(req.body.hasOwnProperty('value') && req.body.hasOwnProperty('phoneID') && req.body.hasOwnProperty('time') &&
-        check(req.body.value).is("number") && check(req.body.phoneID).is("number") ){  // should check date
-
-        postExperiemnt(req, res);
-
-    } else {
-        res.status(400);
-        res.send('POST request to the homepage not successful');
-        console.log('POST was not successful!');
-    }
-});
-
-function postExperiemnt(req, res){
-    Experiment.findOne({"placeID": req.body.placeID}, function(err, element) {
-        if(err) throw err;
-        var avg = d3.mean(req.body.values);
-
-        // Element already exists
-        if(element != null){
-            var newVals = element.values;
-            newVals[req.body.phoneID - 1] = avg;
-            Fingerprint.findOneAndUpdate({"placeID": req.body.placeID},
-                {"values": newVals},
-                function (err, element) {
-                    if (err) throw err;
-                    res.status(200);
-                    res.send('POST request to the homepage successful');
-                    console.log('Update of fingerprint was successful!');
-                });
-        } else {
-            var newVals = [0, 0, 0];
-            newVals[req.body.phoneID - 1] = avg;
-            var newFingerprint = Fingerprint({
-                placeID: req.body.placeID,
-                values: newVals,
-                phoneID: req.body.phoneID
-            });
-
-            newFingerprint.save(function (err) {
-                if (err) throw err;
-                res.status(200);
-                res.send('POST request to the homepage successful');
-                console.log('Saving fingerprint was successful!');
-            });
-        }
-    })
-}
-
 // FIFO queue with extra spices
 function Queue(){
     var queue  = [];
@@ -80,27 +25,63 @@ function Queue(){
 
     this.dequeue = function(){
         if (queue.length == 0) return undefined;
-
         var item = queue[offset];
         offset++;
-
         // Make queue shorter
         if(offset >= maxOffset){
             queue = queue.slice(offset);
             offset = 0;
         }
-
         return item;
     };
 
     // return the item at the specified position
     this.lookInto = function(queueOffset){
-        if(queue[offset + queueOffset] < queue.length){
+        if(offset + queueOffset < queue.length){
             return queue[offset + queueOffset]
         } else {
             return undefined
         }
     };
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('pages/experiment', { title: 'About' });
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+    if(req.body.hasOwnProperty('value') && req.body.hasOwnProperty('phoneID') && req.body.hasOwnProperty('time') &&
+        check(req.body.value).is("number") && check(req.body.phoneID).is("number") ){  // should check date
+
+        postExperiemnt(req, res);
+
+        res.status(200);
+        res.send('POST request to the homepage successful');
+        console.log('POST was successful!');
+    } else {
+        res.status(400);
+        res.send('POST request to the homepage not successful');
+        console.log('POST was not successful!');
+    }
+});
+
+function postExperiemnt(req, res){
+    var values1 = new Queue();
+    var item1 = {"id": 1, "value": -66};
+    var item2 = {"id": 1, "value": -55};
+    var item3 = {"id": 1, "value": -80};
+
+    values1.enqueue(item1);
+    values1.enqueue(item2);
+    values1.enqueue(item3);
+
+    var result1 = values1.dequeue();
+
+    var result2 = values1.lookInto(0);
+    console.log(result1.value);
+    console.log(result2.value);
 }
 
 module.exports = router;
