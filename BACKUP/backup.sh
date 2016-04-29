6324af142c72ba1163d797bcd6d93e3ca5d081a7
root <root@docker.cattivity>
2016-04-29 19:00:55 -0400

    trying to save data

---
+++ BACKUP/backup.sh
@@ -0,0 +1,4 @@
+mongodump --host mongo --db cattivity -c baselines --out /root/BACKUP/data-27-01-2016
+mongodump --host mongo --db cattivity -c fingerprints --out /root/BACKUP/data-27-01-2016
+mongodump --host mongo --db cattivity -c experiments --out /root/BACKUP/data-27-01-2016
+mongodump --host mongo --db cattivity -c places --out /root/BACKUP/data-27-01-2016
